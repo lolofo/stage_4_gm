9cd527b6764a57654b9c8e0945d6e9868e18e5f5
Duc-Hau.NGUYEN <duc-hau.nguyen@irisa.fr>
2022-05-27 14:19:57 +0700

    use tab instead of space

--- training_bert.py
+++ training_bert.py
@@ -1,5 +1,6 @@
 import argparse
 
+import self as self
 import torch
 
 import pytorch_lightning as pl
@@ -130,45 +131,45 @@ class BertNliLight(pl.LightningModule):
 		self.log("test_acc", self.test_acc, on_step=False, on_epoch=True, logger=True)
 
     # return the attention.
-    def get_attention(self,
+	def get_attention(self,
                       input_ids,
                       attention_mask,
                       test_mod: bool = False,
                       *args, **kwargs):
 
-        outputs = self.bert(input_ids=input_ids,
-                            attention_mask=attention_mask,
-                            *args, **kwargs)
-
-        attention_tensor = outputs.attentions
-
-        res = torch.stack(attention_tensor, dim=1)
-
-        # remove the padding tokens
-
-        mask = attention_mask[0, :].detach().numpy() == 1
-        res = res[:, :, :, mask, :]
-        res = res[:, :, :, :, mask]
-
-        tokens = tokenizer.convert_ids_to_tokens(input_ids[0, mask])
-
-        if test_mod:
-            print("test passed : ", end='')
-            passed = True
-            for n in range(len(attention_tensor)):
-                for n_head in range(12):
-                    for x in range(input_ids.shape[1]):
-                        for y in range(input_ids.shape[1]):
-                            if mask[x] == 1 and mask[y] == 1:
-                                if attention_tensor[n][0, n_head, x, y] != res[0, n, n_head, x, y]:
-                                    passed = False
-
-            if passed:
-                print(u'\u2713')
-            else:
-                print("x")
-
-        return res, tokens, input_ids, attention_mask
+	    outputs = self.bert(input_ids=input_ids,
+	                        attention_mask=attention_mask,
+	                        *args, **kwargs)
+	
+	    attention_tensor = outputs.attentions
+	
+	    res = torch.stack(attention_tensor, dim=1)
+	
+	    # remove the padding tokens
+	
+	    mask = attention_mask[0, :].detach().numpy() == 1
+	    res = res[:, :, :, mask, :]
+	    res = res[:, :, :, :, mask]
+	
+	    tokens = tokenizer.convert_ids_to_tokens(input_ids[0, mask])
+	
+	    if test_mod:
+	        print("test passed : ", end='')
+	        passed = True
+	        for n in range(len(attention_tensor)):
+	            for n_head in range(12):
+	                for x in range(input_ids.shape[1]):
+	                    for y in range(input_ids.shape[1]):
+	                        if mask[x] == 1 and mask[y] == 1:
+	                            if attention_tensor[n][0, n_head, x, y] != res[0, n, n_head, x, y]:
+	                                passed = False
+	
+	        if passed:
+	            print(u'\u2713')
+	        else:
+	            print("x")
+	
+	    return res, tokens, input_ids, attention_mask
 
 
 ################
