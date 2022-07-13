3451692abdf2ddaef066087898f3927089918e9b
lolofo <loicfosse65310@gmail.com>
2022-07-13 12:59:25 +0200

    get pickle dict for the inference part

---
+++ get_pickle_inference_dict.py
@@ -0,0 +1,149 @@
+# preparation of the environment
+from os import path
+import os
+import argparse
+import numpy as np
+
+from tqdm import tqdm
+
+from modules.logger import log, init_logging
+import torch
+import pickle
+import json
+
+from torch_set_up import DEVICE
+
+from regularize_training_bert import SNLIDataModule
+from regularize_training_bert import BertNliRegu
+
+
+def get_num_workers() -> int:
+    '''
+    Get maximum logical workers that a machine has
+    Args:
+        default (int): default value
+
+    Returns:
+        maximum workers number
+    '''
+    if hasattr(os, 'sched_getaffinity'):
+        try:
+            return len(os.sched_getaffinity(0))
+        except Exception:
+            pass
+
+    num_workers = os.cpu_count()
+    return num_workers if num_workers is not None else 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # .cache folder >> the folder where everything will be saved
+    cache = path.join(os.getcwd(), '.cache')
+
+    parser.add_argument('-b', '--batch_size', type=int, default=4)
+
+    # default datadir >> ./.cache/dataset >> cache for our datamodule.
+    parser.add_argument('-d', '--data_dir', default=path.join(cache, 'raw_data', 'e_snli'))
+
+    # log_dir for the logger
+    parser.add_argument('-s', '--log_dir', default=path.join(cache, 'logs', 'igrida_trained'))
+
+    parser.add_argument('-n', '--nb_data', type=int, default=-1)
+
+    # config for cluster distribution
+    parser.add_argument('--num_workers', type=int,
+                        default=4)  # auto select appropriate cores in machine
+    parser.add_argument('--accelerator', type=str, default='auto')  # auto select GPU if exists
+
+    # config for the regularization
+    parser.add_argument('--reg_mul', type=float, default=0)  # the regularize terms
+    parser.add_argument('--reg_lay', type=int, default=-1)  # the layer we want to regularize
+    parser.add_argument('--lrate', type=float, default=5e-5)  # the learning rate for the training part
+
+    args = parser.parse_args()
+
+    ckp = path.join(args.log_dir, f"reg_mul={args.reg_mul}", "best.ckpt")
+    model = BertNliRegu.load_from_checkpoint(ckp)
+    model.to(DEVICE)
+    model = model.eval()
+
+    init_logging(
+        color=False,
+        cache_path=os.path.join(args.log_dir, f"reg_mul={args.reg_mul}"),
+        oar_id=f"reg_mul_{args.reg_mul}"
+    )
+
+    log.info(f'>> reg_mul : {args.reg_mul}')
+
+    dm = SNLIDataModule(
+        cache=args.data_dir,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        nb_data=args.nb_data
+    )
+
+    dm.setup(stage="test")
+
+    IDS = []
+    y = []
+    y_hat = []
+
+    d = {}
+    test_loader = dm.test_dataloader()
+    spe_tok = torch.tensor([0, 101, 102]).to(DEVICE)
+
+    with torch.no_grad():
+        for batch in tqdm(test_loader):
+            # loop for the inference on this dict
+            ids = batch["input_ids"].to(DEVICE)
+            annot = batch["annotations"].to(DEVICE)
+            mk = batch["attention_masks"].to(DEVICE)
+            output = model(input_ids=ids,
+                           attention_mask=mk)["outputs"]
+
+            attention_tensor = torch.stack(output.attentions, dim=1)
+            # sum over the lines and the heads
+            cls_lines = attention_tensor[:, :, :, 0, :].sum(dim=1).sum(dim=1) # --> we get the cls line at each
+
+            # replace the specials tokens by zero
+            cls_lines = torch.where(torch.isin(ids, spe_tok), cls_lines, 0)
+
+            buff = cls_lines.clone()
+            buff = torch.where(torch.isin(ids, spe_tok), cls_lines, 1e30) # get the min over the real tokens
+            mins = buff.min(dim=-1)[0].unsqueeze(1).repeat(1, 150)
+            maxs = cls_lines.max(dim=-1)[0].unsqueeze(1).repeat(1, 150)
+
+            cls_lines = (cls_lines - mins) / (maxs - mins)
+            cls_lines = torch.where(torch.isin(ids, spe_tok), cls_lines, 0)
+
+            y_hat.append(cls_lines.flatten())
+            y.append(annot.flatten())
+            IDS.append(ids.flatten())
+
+        y = torch.concat(y, dim=0).cpu().numpy()
+        y_hat = torch.concat(y_hat, dim=0).cpu().numpy()
+
+    log.debug(f">> len(y) : {len(y)}")
+    log.debug(f">> len(y_hat) : {len(y_hat)}")
+    log.debug(f">> ckeck dim : {len(y)==len(y_hat)}")
+
+    idx = np.where(IDS == 0)
+    y = np.delete(y, idx)
+    y_hat = np.delete(y_hat, idx)
+
+    log.info(">> after selection")
+    log.debug(f">> len(y) : {len(y)}")
+    log.debug(f">> len(y_hat) : {len(y_hat)}")
+
+    # creation of the pickle
+    d = {"y": y,
+         "y_hat": y_hat}
+
+    dir = os.path.join(cache, "plots", f"reg_mul={args.reg_mul}", "cls_map.pickle")
+
+    with open(dir, "wb") as f:
+        pickle.dump(d, f)
+
+    log.info(">> DONE !")
